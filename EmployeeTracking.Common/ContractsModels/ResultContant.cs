f432e4e63d61e77a728616a1dd7e85e09be046e8
cagla kirazkaya <57047928+Caglakirazkaya@users.noreply.github.com>
2021-09-20 22:27:36 +0300

    Add files via upload

---
+++ EmployeeTracking.Common/ContractsModels/ResultContant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeTracking.Common.ContractsModels
+{
+    public static class ResultContant
+    {
+        public const string RecordFound = "Kayıt Bulundu";
+        public const string RecordNotFound = "Kayıt Bulunmadı";
+        public const string RecordCreateSuccessfully = "Kayıt Başarıyla Gerçekleşti";
+        public const string RecordCreateNotSuccessfully = "Kayıt Gerçekleştirilirken Hata Oluştu";
+        public const string RecordUpdateSuccessfully = "Kayıt Başarıyla Güncellendi";
+        public const string RecordUpdateNotSuccessfully = "Kayıt Güncellenirken Hata Oluştu";
+    }
+}
---
+++ EmployeeTracking.Common/Mappings/Maps.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EmployeeTarcking.Data.DbModels;
+using EmployeeTracking.Common.ViewModel;
+
+namespace EmployeeTracking.Common.Mappings
+{
+    public class Maps:Profile
+    {
+        public Maps()
+        {
+            CreateMap<EmployeeLeaveType, EmployeeLeaveTypeVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+            CreateMap<EmployeeLeaveAllocation, EmployeeLeaveAllocationVM>().ReverseMap();
+            CreateMap<EmployeeLeaveRequest, EmployeeLeaveRequestVM>().ReverseMap();
+        }
+        
+    }
+}
